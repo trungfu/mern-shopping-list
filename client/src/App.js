ee526a2c103838463defb66db28d8c09a5e912e0
trungtv <tranviettrung92@gmail.com>
2019-05-24 16:18:20 +0700

    using api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppNavBar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -13,7 +15,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavBar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const itemsRoute = require('./routes/api/items');
 
@@ -19,6 +20,15 @@ mongoose.connect(db, {useNewUrlParser: true})
 
 // Use routes
 app.use('/api/items/', itemsRoute)
+
+if(process.env.NODE_ENV === 'production') {
+    // SET static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
